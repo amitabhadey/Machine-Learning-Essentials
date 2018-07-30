520548b7afa1a2a09a56ea92ede8a8d8a778bfdc
AmitabhaDey <amitabhadey1994@gmail.com>
2018-07-31 02:30:54 +0600

    Major Change. Modified directories, updated scripts, added templates.

--- Regression/Simple Linear Regression/simple_linear_regression.py
+++ Regression/Simple Linear Regression/simple_linear_regression.py
@@ -28,21 +28,159 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train) 
 
+
 #Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
 #Visualising the Training set results
 plt.scatter(X_train, y_train, color = 'red')
+
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+
 plt.title('Salary vs Experience (Training set)')
+
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
+
 plt.show()
 
 #Visualising the Test set results
 plt.scatter(X_test, y_test, color = 'red')
+
 plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+
 plt.title('Salary vs Experience (Test set)')
+
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
+
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
